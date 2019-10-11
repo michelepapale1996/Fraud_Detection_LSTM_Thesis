4c7d3fd7d103ab9189e01b19b61b1bf0d94de587
michelepapale1996 <michele.papale@mail.polimi.it>
2019-10-11 19:24:58 +0200

    Started implementing LSTM classifier

--- models/LSTM.py
+++ models/LSTM.py
@@ -4,27 +4,29 @@ from keras.layers.recurrent import LSTM
 from keras.models import Sequential
 from keras.layers.wrappers import TimeDistributed
 from keras.optimizers import Adam, RMSprop
-
 from keras.callbacks import EarlyStopping, Callback, TensorBoard, ModelCheckpoint
 import keras.backend as K
 import shutil
 import os
+import wandb
+from wandb.keras import WandbCallback
+wandb.init(project="fraud-detection-thesis")
 
 
 class VanillaLSTM(object):
-    #layers = {input: 1, 2: 64, 3: 256, 4: 100, output: 1}
+    # layers = {input: 1, 2: 64, 3: 256, 4: 100, output: 1}
     def __init__(self, look_back, layers, dropout, loss, learning_rate):
         self.look_back = look_back
         self.n_hidden = len(layers) - 2
         self.model = Sequential()
         self.layers = layers
         self.loss = loss
-        #self.optimizer = optimizer
+        # self.optimizer = optimizer
         self.learning_rate = learning_rate
         self.dropout = dropout
 
     def build_model(self):
-        #first add input to hidden1
+        # first add input to hidden1
         self.model.add(LSTM(
             self.layers['input'],
             input_shape=(self.look_back, self.layers['input']),
@@ -38,29 +40,59 @@ class VanillaLSTM(object):
         # add output
         self.model.add(Dense(self.layers['output']))
 
-        #compile model and print summary
+        # compile model and print summary
         self.model.compile(loss=self.loss, optimizer=Adam(lr=self.learning_rate))
         self.model.summary()
         return self.model
 
 
-def train_model(model, x_train, y_train, batch_size, epochs, shuffle, validation, validation_data, patience):
-    print("Training")
+class LstmClassifier(object):
+    def __init__(self, look_back, layers, dropout, loss, learning_rate, num_features, metrics):
+        self.look_back = look_back
+        self.n_hidden = len(layers) - 2
+        self.model = Sequential()
+        self.layers = layers
+        self.loss = loss
+        # self.optimizer = optimizer
+        self.learning_rate = learning_rate
+        self.dropout = dropout
+        self.num_features = num_features
+        self.metrics = metrics
+
+    def build_model(self):
+        # first add input to hidden1
+        self.model.add(LSTM(
+            self.layers['input'],
+            input_shape=(self.look_back + 1, self.num_features),
+            return_sequences=True,
+            ))
+
+        self.model.add(LSTM(self.layers['hidden1'], return_sequences=False))
+
+        # add output
+        self.model.add(Dense(self.layers['output'], activation='sigmoid'))
+
+        # compile model and print summary
+        self.model.compile(loss=self.loss, optimizer=Adam(lr=self.learning_rate), metrics=self.metrics)
+        # self.model.summary()
+        return self.model
+
+
+def train_model(model, x_train, y_train, batch_size, epochs, shuffle=False, validation=False, patience=0):
+    print("Training...")
     training_start_time = time.time()
-    # tensorboard = TensorBoard(log_dir='/home/esihakh/projects/github_thesis/lstm_anomaly_thesis/logs/tf_logs/', histogram_freq=5, write_graph=True, write_images=False)
+
     if validation:
         early_stopping = EarlyStopping(monitor='val_loss', patience=patience)
-
-        history_callback = model.fit(x_train, y_train, batch_size=batch_size, epochs=epochs,
-                                     validation_data=validation_data,
-                                     shuffle=shuffle, verbose=2, callbacks=[early_stopping])
+        history_callback = model.fit(x_train, y_train, batch_size=batch_size, epochs=epochs, shuffle=shuffle, verbose=2, callbacks=[early_stopping])
     else:
-        history_callback = model.fit(x_train, y_train, epochs=epochs, verbose=2)
-    print('Training duration (s) : %s', str(time.time() - training_start_time))
-    print("Training Loss per epoch: %s" % str(history_callback.history["loss"]))
+        history_callback = model.fit(x_train, y_train, epochs=epochs, verbose=2, callbacks=[WandbCallback()], shuffle=shuffle)
+        model.save(os.path.join(wandb.run.dir, "model.h5"))
+
+    print('Training done. Duration: ' + str(time.time() - training_start_time))
+    print("Training Loss per epoch: " + str(history_callback.history["loss"]))
     if validation:
         print("Validation  Loss per epoch: %s" % str(history_callback.history["val_loss"]))
-    print(history_callback.history.keys())
     return history_callback
     # for epoch in range(epochs):
     #     model.fit(x_train, y_train, batch_size=batch_size, epochs=1,shuffle=shuffle, verbose=2)
