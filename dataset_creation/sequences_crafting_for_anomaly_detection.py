1cbaa7a02ae93048039ec275196cae3abe3f79ce
michelepapale1996 <michele.papale@mail.polimi.it>
2019-11-03 11:33:32 +0100

    Improved sequences creation (and dividid by goal - classification/prediction)

--- dataset_creation/sequences_crafting_for_anomaly_detection.py
+++ dataset_creation/sequences_crafting_for_anomaly_detection.py
@@ -15,28 +15,39 @@ warnings.filterwarnings("ignore")
 # ---------------------- Module used to create the dataset given to the LSTM --------------------
 # -----------------------------------------------------------------------------------------------
 
-
 # creates samples starting by the sequence of transactions of a given user
 # NB: only the last transaction of a sequence can be a fraud
 # returns numpy array
 def get_sequences(transactions, look_back):
-    cols = ['Importo', 'NumConfermaSMS', 'isItalianSender', 'isItalianReceiver']
+    cols = ['Importo',
+            'NumConfermaSMS',
+            'isItalianSender',
+            'isItalianReceiver',
+            'count_trx_iban',
+            'is_new_asn_cc',
+            'is_new_iban',
+            'is_new_iban_cc',
+            'is_new_ip']
     genuine_transactions = transactions.loc[transactions.isFraud == 0, cols]
     sequences = []
-    labels = []
+    next_transaction = []
 
     pointer = 0
+    # NB: in look_back transactions there must not be present frauds
     while pointer < len(genuine_transactions.index) - look_back:
         sequence = genuine_transactions[pointer: pointer + look_back]
         index_last_genuine_transaction = genuine_transactions.index[pointer + look_back - 1]
 
         # get the next transaction
         i = transactions.index.get_loc(index_last_genuine_transaction)
-        sequence = sequence.append(transactions.iloc[i + 1][cols])
-        labels.append(transactions.iloc[i + 1]["isFraud"])
-        sequences.append(sequence.values)
+        try:
+            sequence = sequence.append(transactions.iloc[i + 1][cols])
+            next_transaction.append(transactions.iloc[i + 1][cols + ["isFraud"]])
+            sequences.append(sequence.values)
+        except Exception as e:
+            print(e)
         pointer += 1
-    return sequences, labels
+    return sequences, next_transaction
 
 
 # converting low level dataset (transactions of all users)
@@ -54,10 +65,12 @@ def create_dataset(dataset, look_back=1):
         transactions = bonifici_by_user.get_group(user).sort_values("Timestamp")
         transactions = transactions.drop(["UserID", "Timestamp"], axis=1)
         sequence_x, sequence_y = get_sequences(transactions, look_back)
+
         x.extend(sequence_x)
         y.extend(sequence_y)
 
         number_of_users += 1
+        print(number_of_users)
         number_of_sequences += len(sequence_y)
 
     print("There are " + str(number_of_sequences) + " sequences with " + str(number_of_users) + " users.")
@@ -80,4 +93,11 @@ print("Preparing training and test sets...")
 bonifici = pd.read_csv("../datasets/bonifici_engineered.csv", parse_dates=True)
 bonifici.set_index('indice', inplace=True)
 bonifici = bonifici.drop(["IDSessione", "IP", "IBAN", "IBAN_CC", "CC_ASN"], axis=1)
-x, y = create_dataset(bonifici, 25)
+print("Creating datasets...")
+look_back = 49
+x, y = create_dataset(bonifici, look_back)
+
+print("Saving dataset...")
+np.save("/home/mpapale/thesis/anomaly_detection/dataset_" + str(look_back) + "_lookback/x.npy", x)
+np.save("/home/mpapale/thesis/anomaly_detection/dataset_" + str(look_back) + "_lookback/y.npy", y)
+print("Done.")
