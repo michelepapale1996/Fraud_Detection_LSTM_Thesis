b08ca90f7989e049bc1f8f4da0a47e3750d51308
michelepapale1996 <michele.papale@mail.polimi.it>
2019-11-03 11:35:15 +0100

    LSTM_classifier minor updates

--- models/LSTM_classifier.py
+++ models/LSTM_classifier.py
@@ -7,7 +7,7 @@ import seaborn as sns
 import warnings
 from keras.wrappers.scikit_learn import KerasClassifier
 from sklearn.model_selection import GridSearchCV
-from sklearn.metrics import f1_score, roc_curve, roc_auc_score, precision_recall_curve, auc, make_scorer
+from sklearn.metrics import f1_score, roc_curve, roc_auc_score, precision_recall_curve, auc, make_scorer, confusion_matrix, accuracy_score
 from sklearn.model_selection import RandomizedSearchCV
 from keras.layers.recurrent import LSTM
 from keras.layers.core import Dense, Activation, Dropout, RepeatVector
@@ -31,14 +31,16 @@ np.random.seed(seed)
 # --------------------------Main idea: creating an LSTM classifier ------------------------------
 # -----------------------------------------------------------------------------------------------
 
+look_back = 9
+num_features = 9
 
 def undersample(x, y):
     num_frauds = np.unique(y, return_counts=True)[1][1]
     x_no_frauds = x[np.where(y == 0)]
-    x_resampled = np.zeros((0, 51, 4))
+    x_resampled = np.zeros((0, look_back + 1, num_features))
     y_resampled = np.zeros(0)
-    for _ in range(num_frauds):
-        reshaped = np.reshape(x_no_frauds[random.randint(0, len(x_no_frauds))], (1, 51, 4))
+    for _ in range(num_frauds * 10):
+        reshaped = np.reshape(x_no_frauds[random.randint(0, len(x_no_frauds) - 1)], (1, look_back + 1, num_features))
         x_resampled = np.concatenate((x_resampled, reshaped))
         y_resampled = np.append(y_resampled, 0)
     x_resampled = np.concatenate((x_resampled, x[np.where(y == 1)]))
@@ -48,52 +50,49 @@ def undersample(x, y):
     return x_resampled, y_resampled
 
 
-x = np.load("dataset_50_lookback/x.npy")
-y = np.load("dataset_50_lookback/y.npy")
+x = np.load("/home/mpapale/thesis/classification/dataset_" + str(look_back) + "_lookback/x.npy")
+y = np.load("/home/mpapale/thesis/classification/dataset_" + str(look_back) + "_lookback/y.npy")
+print("Il ratio frodi vs genuine è: ")
+unique, counts = np.unique(y, return_counts=True)
+print(counts)
 x, y = undersample(x, y)
+print("Dopo l'undersampling il ratio frodi vs genuine è: ")
+unique, counts = np.unique(y, return_counts=True)
+print(counts)
 
-look_back = 50
-num_features = 4
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.33, random_state=42)
+print("train_labels")
+print(np.unique(y_train, return_counts=True))
+print("test_labels")
+print(np.unique(y_test, return_counts=True))
 
 # define the grid search parameters
-batch_size = [10, 50, 100]
-epochs = [10, 50, 100]
-layers = [{'input': 10, 'output': 1},
-          {'input': 128, 'output': 1},
-          {'input': 256, 'output': 1},
-          {'input': 10, 'hidden1': 10, 'output': 1},
-          {'input': 256, 'hidden1': 128, 'output': 1}]
+batch_size = [1, 5]
+epochs = [10]
+layers = [{'input': 10, 'output': 1}]#,
+          #{'input': 128, 'output': 1},
+          #{'input': 128, 'hidden1': 64, 'output': 1},
+          #{'input': 256, 'hidden1': 128, 'output': 1},
+          #{'input': 512, 'hidden1': 256, 'hidden2': 128, 'output': 1}]
 learning_rate = [0.1, 0.01, 0.001]
-dropout_rate = [0.0, 0.3, 0.6]
+dropout_rate = [0.0, 0.3, 0.8]
 
 
-def create_model(layers={'input': 5, 'output': 1}, learning_rate=0.01, dropout_rate=0, look_back=50):
+def create_model(layers={'input': 5, 'output': 1}, learning_rate=0.01, dropout_rate=0, look_back=25):
     model = Sequential()
     n_hidden = len(layers) - 2
     if n_hidden > 2:
-        model.add(LSTM(
-            layers['input'],
-            input_shape=(look_back + 1, num_features),
-            return_sequences=True,
-        ))
+        model.add(LSTM(layers['input'], input_shape=(look_back + 1, num_features), return_sequences=True))
 
         # add hidden layers return sequence true
         for i in range(2, n_hidden):
-            model.add(LSTM(
-                layers["hidden" + str(i)],
-                return_sequences=True))
+            model.add(LSTM(layers["hidden" + str(i)], return_sequences=True))
             model.add(Dropout(dropout_rate))
         # add hidden_last return Sequences False
-        model.add(LSTM(
-            layers['hidden' + str(n_hidden)],
-            return_sequences=False))
+        model.add(LSTM(layers['hidden' + str(n_hidden)], return_sequences=False))
         model.add(Dropout(dropout_rate))
     else:
-        model.add(LSTM(
-            layers['input'],
-            input_shape=(look_back + 1, num_features),
-            return_sequences=False,
-        ))
+        model.add(LSTM(layers['input'], input_shape=(look_back + 1, num_features), return_sequences=False))
 
     # add output
     model.add(Dense(layers['output'], activation='sigmoid'))
@@ -106,32 +105,29 @@ def create_model(layers={'input': 5, 'output': 1}, learning_rate=0.01, dropout_r
 
 def evaluate_performance(y_true, y_pred):
     print("---- Performance ----")
-
     roc_auc = roc_auc_score(y_true, y_pred)
-    print("Roc-auc score: %.3f" % roc_auc)
-    # plot the roc curve for the model
-    fpr, tpr, _ = roc_curve(y_true, y_pred)
+    print("Roc_auc score: %.3f" % roc_auc)
 
     precision, recall, thresholds = precision_recall_curve(y_true, y_pred)
-    precision_recall_auc = auc(recall, precision)
-    print("Precision_recall_auc score: %.3f" % precision_recall_auc)
-
     def get_position(recall):
         for i in range(0, len(recall)):
             if recall[i] < 0.2:
-                return i
-        return len(recall) - 1
+                return i - 1
 
     # This function adjusts class predictions based on the prediction threshold (t).
     def adjusted_classes(y_scores, t):
         return [1 if y >= t else 0 for y in y_scores]
+
+    precision_recall_auc = auc(recall, precision)
+    print("Precision_recall_auc score: %.3f" % precision_recall_auc)
     position = get_position(recall)
-    print("la dimensione è: " + str(len(thresholds)))
+    print("Precision: " + str(precision[position]))
     threshold = thresholds[position]
     y_pred = adjusted_classes(y_pred, threshold)
-
     f1 = f1_score(y_true, y_pred)
     print("f1 score: %.3f" % f1)
+    print("accuracy: " + str(accuracy_score(y_true, y_pred)))
+    print(confusion_matrix(y_true, y_pred))
     return f1
 
 
@@ -147,14 +143,17 @@ grid = RandomizedSearchCV(estimator=model,
                           param_distributions=param_grid,
                           n_jobs=-1,
                           cv=3,
-                          n_iter=50,
-                          verbose=1,
+                          n_iter=2,
+                          verbose=2,
                           scoring=scoring)
-grid_result = grid.fit(x, y)
+grid_result = grid.fit(x_train, y_train)
 # summarize results
-print("Best: %f using %s" % (grid_result.best_score_, grid_result.best_params_))
+print("Best f1: %f using %s" % (grid_result.best_score_, grid_result.best_params_))
 means = grid_result.cv_results_['mean_test_score']
 stds = grid_result.cv_results_['std_test_score']
 params = grid_result.cv_results_['params']
 for mean, stdev, param in zip(means, stds, params):
     print("%f (%f) with: %r" % (mean, stdev, param))
+
+print("Best model score on test set")
+grid_result.score(x_test, y_test)
