586496dfa81406ba275d04019b63b74982057a0c
michelepapale1996 <michele.papale@mail.polimi.it>
2019-11-03 11:32:44 +0100

    Added new features

--- dataset_creation/feature_engineering.py
+++ dataset_creation/feature_engineering.py
@@ -13,14 +13,13 @@ pd.set_option('display.max_columns', 100)
 sns.set(style="white", color_codes=True)
 sns.set_context(rc={"font.family":'sans',"font.size":24,"axes.titlesize":24,"axes.labelsize":24})
 
-
 # -----------------------------------------------------------------------------------------------
 # -------------------------- Feature engineering module -----------------------------------------
 # -----------------------------------------------------------------------------------------------
 
 # reading the datasets
-bonifici = pd.read_csv("datasets/quiubi_bonifici.csv")
-segnalaz = pd.read_csv("datasets/bonifici_segnalaz.csv")
+bonifici = pd.read_csv("/home/mpapale/thesis/datasets/quiubi_bonifici.csv")
+segnalaz = pd.read_csv("/home/mpapale/thesis/datasets/bonifici_segnalaz.csv")
 bonifici.set_index('indice',inplace=True)
 segnalaz.set_index('indice',inplace=True)
 
@@ -28,8 +27,9 @@ segnalaz.set_index('indice',inplace=True)
 useless_features = ["CAP", "Servizio", "Status", "Paese", "Provincia", "Nazione", "IDTransazione", "CRO", "Causale", "Valuta", "ProfSicurezza", "NumConto", "ABI", "CAB", "Intestatario", "Indirizzo"]
 bonifici = bonifici.drop(useless_features, axis=1)
 segnalaz = segnalaz.drop(useless_features, axis=1)
-bonifici = bonifici.drop(["DataValuta", "DataEsecuzione", "MsgErrore", "Nominativo", "TipoOperazione"], axis=1)
-segnalaz = segnalaz.drop(["DataValuta", "DataEsecuzione", "MsgErrore", "Nominativo", "TipoOperazione"], axis=1)
+# in future, try to use these features
+bonifici = bonifici.drop(["DataValuta", "DataEsecuzione", "Nominativo", "TipoOperazione"], axis=1)
+segnalaz = segnalaz.drop(["DataValuta", "DataEsecuzione", "Nominativo", "TipoOperazione"], axis=1)
 
 # datasets merge into bonifici
 bonifici["isFraud"] = np.zeros(len(bonifici.index))
@@ -37,23 +37,35 @@ for index, row in segnalaz.iterrows():
     if index in bonifici.index:
         bonifici.loc[index, "isFraud"] = 1
     else:
-        # print(index)
         bonifici.append(row)
 bonifici["isFraud"] = pd.to_numeric(bonifici["isFraud"], downcast='integer')
 
 bonifici.Timestamp = pd.to_datetime(bonifici.Timestamp)
 bonifici.NumConfermaSMS = bonifici.NumConfermaSMS.eq('Si').astype(int)
 
+
+print("Rescaling importo...")
 # rescaling "Importo"
 x = bonifici[["Importo"]].to_numpy()
 min_max_scaler = preprocessing.MinMaxScaler()
 x_scaled = min_max_scaler.fit_transform(x)
 bonifici[["Importo"]] = x_scaled
 
-# creating "isItalianSender" and "isItalianReceiver"
-bonifici.loc[:, "isItalianSender"] = np.ones(len(bonifici.index))
-bonifici.loc[:, "isItalianReceiver"] = np.ones(len(bonifici.index))
+
+print("Changing MsgErrore to boolean...")
+# MsgErrore changed to boolean
+bonifici.MsgErrore.fillna(0, inplace=True)
+bonifici["MsgErrore"] = bonifici["MsgErrore"].apply(lambda x : 1 if x != 0 else 0)
+
+
+print("Creating isItalianSender and isItalianReceiver...")
+# creating "isItalianSender", "isItalianReceiver"
+bonifici["isItalianSender"] = np.ones(len(bonifici.index))
+bonifici["isItalianReceiver"] = np.ones(len(bonifici.index))
+i = 0
 for index, row in bonifici[["CC_ASN", "IBAN_CC"]].iterrows():
+    print(i)
+    i += 1
     if row["CC_ASN"][:2] != "IT":
         bonifici.at[index, "isItalianSender"] = 0
     if row["IBAN_CC"] != "IT":
@@ -61,5 +73,59 @@ for index, row in bonifici[["CC_ASN", "IBAN_CC"]].iterrows():
 bonifici["isItalianSender"] = pd.to_numeric(bonifici["isItalianSender"], downcast='integer')
 bonifici["isItalianReceiver"] = pd.to_numeric(bonifici["isItalianReceiver"], downcast='integer')
 
+
+# todo: creare timestamp delta di quanto tempo è passato dall'ultima tranasazione
+
+print("Creating count_trx_iban, is_new_asn_cc, is_new_iban, is_new_iban_cc, is_new_ip...")
+# creating count_trx_iban, is_new_asn_cc, is_new_iban, is_new_iban_cc
+bonifici_by_user = bonifici.groupby("UserID")
+
+bonifici["count_trx_iban"] = np.zeros(len(bonifici.index))
+bonifici["is_new_asn_cc"] = np.zeros(len(bonifici.index))
+bonifici["is_new_iban"] = np.zeros(len(bonifici.index))
+bonifici["is_new_iban_cc"] = np.zeros(len(bonifici.index))
+bonifici["is_new_ip"] = np.zeros(len(bonifici.index))
+# for each transaction of the user:
+    #   - count the number of preceding transactions to the same iban
+    #   - check if the asn_cc is new
+    #   - check if the iban receiver is new
+    #   - check if the iban_cc is new
+    #   - check if the ip of the user is new
+counter = 0
+for user in bonifici_by_user.groups.keys():
+    print(counter)
+    counter += 1
+    # order by time
+    group = bonifici_by_user.get_group(user).sort_values(by='Timestamp', ascending=True)
+
+    for i in range(len(group)):
+        count_trx_iban = 0
+        is_new_asn_cc = 1
+        is_new_iban = 1
+        is_new_iban_cc = 1
+        is_new_ip = 1
+        for j in range(0, i):
+            if group.iloc[i]["IBAN"] == group.iloc[j]["IBAN"]:
+                count_trx_iban += 1
+            if group.iloc[i]["CC_ASN"] == group.iloc[j]["CC_ASN"]:
+                is_new_asn_cc = 0
+            if group.iloc[i]["IBAN"] == group.iloc[j]["IBAN"]:
+                is_new_iban = 0
+            if group.iloc[i]["IBAN_CC"] == group.iloc[j]["IBAN_CC"]:
+                is_new_iban = 0
+            if group.iloc[i]["IP"] == group.iloc[j]["IP"]:
+                is_new_ip = 0
+        bonifici.at[group.iloc[i:i + 1].index[0], "count_trx_iban"] = count_trx_iban
+        bonifici.at[group.iloc[i:i + 1].index[0], "is_new_asn_cc"] = is_new_asn_cc
+        bonifici.at[group.iloc[i:i + 1].index[0], "is_new_iban"] = is_new_iban
+        bonifici.at[group.iloc[i:i + 1].index[0], "is_new_iban_cc"] = is_new_iban_cc
+        bonifici.at[group.iloc[i:i + 1].index[0], "is_new_ip"] = is_new_ip
+
+bonifici["count_trx_iban"] = pd.to_numeric(bonifici["count_trx_iban"], downcast='integer')
+bonifici["is_new_asn_cc"] = pd.to_numeric(bonifici["is_new_asn_cc"], downcast='integer')
+bonifici["is_new_iban"] = pd.to_numeric(bonifici["is_new_iban"], downcast='integer')
+bonifici["is_new_iban_cc"] = pd.to_numeric(bonifici["is_new_iban_cc"], downcast='integer')
+bonifici["is_new_ip"] = pd.to_numeric(bonifici["is_new_ip"], downcast='integer')
+
 # saving the dataset
-bonifici.to_csv("datasets/bonifici_engineered.csv")
+bonifici.to_csv("/home/mpapale/thesis/datasets/bonifici_engineered.csv")
